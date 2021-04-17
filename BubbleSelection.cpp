e0842da21cbe54bbe61dd5fa2ba62a0a07887ff6
Yago Marialva Bentes <ymb@icomp.ufam.edu.br>
2021-04-17 10:24:24 -0400

    Add files via upload

---
+++ BubbleSelection.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+//A funçºao de bubble sort
+void bubbleSort(int arr[], int n)
+{
+	int i, j,k;
+	
+	for (i = 0; i < n; i++)	
+	printf("%d movimentacao em bubble\n",i+1);
+	// Se o i ja estiver no final
+	for (j = 0; j < n-i-1; j++)
+		if (arr[j] > arr[j+1])
+			swap(&arr[j], &arr[j+1]);	
+	k++;		
+}
+
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+    	
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+        if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+        swap(&arr[min_idx], &arr[i]);
+			printf("%d movimentacao em selection\n",i+1); 
+    } 
+} 
+ 
+
+//funcao para printar a lista
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// DExibir o codigo
+int main()
+{
+	int arr[] = {10,8,6,4,2,1,3};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	bubbleSort(arr, n);
+	printf("=============================================== \n");
+	selectionSort(arr, n);
+	cout<<"Ordenado o vetor: \n";
+	printArray(arr, n);
+	return 0;
+}
